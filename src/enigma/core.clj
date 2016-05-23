84b3c18297c6509470d589d49652ceee8ba71b47
Niall Robinson <niall.h.robinson@gmail.com>
2016-05-23 18:20:20 -0400

    some basic rotoring working

--- src/enigma/core.clj
+++ src/enigma/core.clj
@@ -1,14 +1,15 @@
 (ns enigma.core
-  (:gen-class))
+  (:gen-class)
+  (:use [debux core]))
 
 (defn -main
   [& args]
   (println "Hello, World!"))
 
 (def rotors {
-    "I"     : (with-meta "EKMFLGDQVZNTOWYHXUSPAIBRCJ" {:nudge "R" :keyval "E"},
-    "II"    : (with-meta "AJDKSIRUXBLHWTMCQGZNPYFVOE" {:nudge "F" :keyval "A"};,
-    ; "III"   : { mapper: "BDFHJLCPRTXVZNYEIWGAKMUSQO", step: "W"},
+    :I (with-meta (seq "EKMFLGDQVZNTOWYHXUSPAIBRCJ") {:nudge \R :rotation 0}),
+    :II (with-meta (seq "AJDKSIRUXBLHWTMCQGZNPYFVOE") {:nudge \F :rotation 0}),
+    :III (with-meta (seq "BDFHJLCPRTXVZNYEIWGAKMUSQO") {:nudge \W :rotation 0});,
     ; "IV"    : { mapper: "ESOVPZJAYQUIRHXLNFTGKDCMWB", step: "K"},
     ; "V"     : { mapper: "VZBRGITYUPSDNHLXAWMJQOFECK", step: "A"},
     ; "VI"    : { mapper: "JPGVOUMFYQBENHZRDKASXLICTW", step: "AN"},
@@ -19,39 +20,61 @@
   })
 
 (def reflectors {
-    "B":    [:A "Y", :B "R", :C "U", :D "H", :E "Q", :F "S", :G "L", :I "P", :J "X", :K "N", :M "O", :T "Z", :V "W"];,
+    :B {:A "Y", :B "R", :C "U", :D "H", :E "Q", :F "S", :G "L", :I "P", :J "X", :K "N", :M "O", :T "Z", :V "W"};,
     ; "C":    ['AF', 'BV', 'CP', 'DJ', 'EI', 'GO', 'HY', 'KR', 'LZ', 'MX', 'NW', 'TQ', 'SU'],
     ; "B Dünn":   ['AE', 'BN', 'CK', 'DQ', 'FU', 'GY', 'HW', 'IJ', 'LO', 'MP', 'RX', 'SZ', 'TV'],
     ; "C Dünn":   ['AR', 'BD', 'CO', 'EJ', 'FN', 'GT', 'HK', 'IV', 'LM', 'PW', 'QZ', 'SX', 'UY']
   })
 
 (defn rotate-rotor
+  ([rotor]
+   (rotate-rotor rotor 1))
+  ([rotor nsteps]
+   (let [rotation (:rotation (meta rotor))]
+    (vary-meta rotor assoc :rotation (mod (+ rotation nsteps) (count rotor)))
+   )
+  )
+)
+
+(defn nudge?
   [rotor]
-  (let [mapper (:mapper rotor)
-        step (:step rotor)]
-    {:mapper (str (apply str (rest a)) (first a))
-     :step step}
+  (= 
+    (nth rotor (:rotation (meta rotor)))
+    (:nudge (meta rotor))
     )
+  )
+
+(defn rotate-rotors
+  ([rotors]
+    (rotate-rotors (first rotors) (rest rotors) []))
+  ([rotor rotors rotated-rotors]
+    (let [updated-rotors (conj rotated-rotors (rotate-rotor rotor))]
+      (if (and (seq rotors) (nudge? rotor))
+        (rotate-rotors (first rotors) (rest rotors) updated-rotors)
+        (into updated-rotors rotors)
+        )
+    )
+  )
 )
 
 (defn char->int
   [c]
-  (- (int c) 97) ;to get a->1, b->2 etc
+  (- (int c) 65) ;to get a->1, b->2 etc
   )
 
 (defn rotor-encode
-  [letter rotor]
-  (nth rotor (char->int letter))
+  [rotor letter]
+  (nth rotor (mod (+ (:rotation (meta rotor)) (char->int letter)) (count rotor)))
   )
 
 (defn rotors-encode
   ([rotors letter]
-    (recur letter (first rotors) (last rotors)))
+    (rotors-encode (first rotors) (rest rotors) letter))
   ([rotor rotors letter]
-   (let [newletter (rotor-encode letter)]
+   (let [nextletter (rotor-encode rotor letter)]
      (if (empty? rotors)
-      newletter
-      (recur (first rotors) (rest rotors) newletter))))
+      nextletter
+      (recur (first rotors) (rest rotors) nextletter))))
   )
 
 (defn reflect
@@ -65,10 +88,10 @@
         (partial rotors-encode (reverse rotors)))
   )
 
-(defn rotate
-  ([rotors]
-   (recur (first rotors) (rest rotors)) [])
-  ([rotor rotors rotated-rotors]
-   (let [rotated-rotor (rotate-rotor rotor)]
-     (if rotated-rotor)))
-  )
+; (defn rotate
+;   ([rotors]
+;    (rotate (first rotors) (rest rotors)) [])
+;   ([rotor rotors rotated-rotors]
+;    (let [rotated-rotor (rotate-rotor rotor)]
+;      (if rotated-rotor)))
+;   )
--- test/enigma/core_test.clj
+++ test/enigma/core_test.clj
@@ -1,7 +1,46 @@
 (ns enigma.core-test
   (:require [clojure.test :refer :all]
-            [enigma.core :refer :all]))
+            [enigma.core :refer :all])
+  (:use [debux core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-char->int
+  (testing "Char as int in the alphabet")
+  (is (= (char->int \A) 0))
+  (is (= (char->int \Z) 25))
+  )
+
+(deftest test-rotate-rotor
+  (testing "Update the rotor key with the rotation")
+  (is (= 
+        (:rotation (meta (rotate-rotor (:I rotors))))
+        1
+    ))
+  (is (= 
+        (:rotation (meta (rotate-rotor (:I rotors) 28)))
+        2
+    ))
+  )
+
+(deftest test-rotor-encode
+  (testing "Make sure a rotor encodes a char to the correct letter")
+  (is (=
+        (rotor-encode (:I rotors) \B)
+        \K
+        )))
+
+(deftest test-rotors-encode
+  (testing "Right letter with multiple rotors")
+  (is (=
+        (rotors-encode [(:I rotors) (:II rotors) (:III rotors)] \A)
+        \G
+        )))
+
+(deftest test-rotate-rotors
+  (testing "Sequence of rotors and nudges")
+  (is (= (let [rotated-rotors (rotate-rotors [(:I rotors) (:II rotors) (:III rotors)])]
+           (map #(:rotation (meta %)) rotated-rotors)
+         )
+       [1 0 0]
+      )
+  )
+)
